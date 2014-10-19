e65dd555a7cc73d10090ab5c3cc889ff5470d5cf
Ken Paulson <ken@muffinmangames.com>
2014-10-19 04:53:33 -0400

    No longer using SVN so "trunk" isn't a valid path

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
-xcopy "c:\Projects\WoW\AutoBar\trunk" "E:\World of Warcraft\Interface\Addons\AutoBar" /Y /S /EXCLUDE:xcopy_ignore.txt
+xcopy "c:\Projects\WoW\AutoBar" "E:\World of Warcraft\Interface\Addons\AutoBar" /Y /S /EXCLUDE:xcopy_ignore.txt
 
 pause
