3f5c277784fd2edff1fca707747829ba80789e43
Ken Paulson <ken@muffinmangames.com>
2014-11-15 18:37:20 -0500

    Added LipPT to the updated libs

--- UpdateAutoBar.bat
+++ UpdateAutoBar.bat
@@ -1,3 +1,7 @@
 xcopy "c:\Projects\WoW\MuffinLibPTSets" "c:\Projects\WoW\AutoBar\libs\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
 
+xcopy "c:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Consumable" "c:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Consumable" /Y /S /I
+xcopy "c:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Misc" "c:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Misc" /Y /S /I
+xcopy "c:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Tradeskill" "c:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Tradeskill" /Y /S /I
+
 pause
