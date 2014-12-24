267859c014438074553ca5a728b5c074476247d2
Ken Paulson <ken@muffinmangames.com>
2014-12-24 01:22:48 -0500

    Updated paths for new dev machine

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
-xcopy "c:\Projects\WoW\AutoBar" "E:\World of Warcraft\Interface\Addons\AutoBar" /Y /S /EXCLUDE:xcopy_ignore.txt
+xcopy "D:\Projects\WoW\AutoBar" "D:\Apps\World of Warcraft\Interface\Addons\AutoBar" /Y /S /EXCLUDE:xcopy_ignore.txt
 
 pause
