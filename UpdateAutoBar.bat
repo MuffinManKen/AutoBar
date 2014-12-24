850e2d36fbe137af64e7732c540b1af535fe5fcf
Ken Paulson <ken@muffinmangames.com>
2014-12-24 01:22:33 -0500

    Updated paths for new dev machine

--- UpdateAutoBar.bat
+++ UpdateAutoBar.bat
@@ -1,7 +1,7 @@
-xcopy "c:\Projects\WoW\MuffinLibPTSets" "c:\Projects\WoW\AutoBar\libs\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
+xcopy "D:\Projects\WoW\MuffinLibPTSets" "D:\Projects\WoW\AutoBar\libs\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
 
-xcopy "c:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Consumable" "c:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Consumable" /Y /S /I
-xcopy "c:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Misc" "c:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Misc" /Y /S /I
-xcopy "c:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Tradeskill" "c:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Tradeskill" /Y /S /I
+xcopy "D:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Consumable" "D:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Consumable" /Y /S /I
+xcopy "D:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Misc" "D:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Misc" /Y /S /I
+xcopy "D:\Projects\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Tradeskill" "D:\Projects\WoW\AutoBar\libs\LibPeriodicTable-3.1-Tradeskill" /Y /S /I
 
 pause
