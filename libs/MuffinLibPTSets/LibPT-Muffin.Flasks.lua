4372ccefbe5cc64bdfc6cdfb81f7724048607608
Ken Paulson <ken@muffinmangames.com>
2015-01-04 23:59:07 -0500

    updated libs

--- libs/MuffinLibPTSets/LibPT-Muffin.Flasks.lua
+++ libs/MuffinLibPTSets/LibPT-Muffin.Flasks.lua
@@ -1,6 +1,8 @@
 --
 -- LibPT-Muffin.Flasks
 --
+-- ToDo: Need these broken down by stat (Strength, Agility, etc)
+--
 if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
 LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Flasks", "Rev: 3",
 {
