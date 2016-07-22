b52d31df11e48d575c9e43129b78515353254ba5
Ken Paulson <ken@muffinmangames.com>
2016-07-22 15:42:48 -0400

    Adds a "nice class" version of the AutoBar.CLASS member. First letter is capitalized, rest is lower case. To be used with class-specific spell sets

--- Core.lua
+++ Core.lua
@@ -255,6 +255,7 @@ function AutoBar:OnInitialize()
 
 	AutoBar.currentPlayer = UnitName("player") .. " - " .. GetRealmName();
 	_, AutoBar.CLASS = UnitClass("player")
+	AutoBar.NiceClass = string.sub(AutoBar.CLASS, 1, 1) .. string.lower(string.sub(AutoBar.CLASS, 2))
 	AutoBar.CLASSPROFILE = "_" .. AutoBar.CLASS;
 
 	AutoBar:RegisterDB("AutoBarDB", nil, "class")
