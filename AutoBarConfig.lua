9f584a47e42a01733d95c64ab8307dad7bf70075
Ken Paulson <ken@muffinmangames.com>
2016-09-06 09:37:10 -0400

    Playing with Ace3 GUI

--- AutoBarConfig.lua
+++ AutoBarConfig.lua
@@ -7,42 +7,49 @@
 -- http://muffinmangames.com
 --
 
---	PeriodicGroup
---		description
---		texture
---		targeted
---		nonCombat
---		battleground
---		notUsable (soul shards, arrows, etc.)
---		flying
-
---	AutoBar
---		spell
---		limit
+local AceGUI = LibStub("AceGUI-3.0")
 
 local AutoBar = AutoBar
 
 local L = AutoBar.locale
 local _
 
-AutoBar.panel = CreateFrame( "Frame", "AutoBarConfigPanel", UIParent );
+--AutoBar.panel = CreateFrame( "Frame", "AutoBarConfigPanel", UIParent );
 -- Register in the Interface Addon Options GUI
 -- Set the name for the Category for the Options Panel
-AutoBar.panel.name = "AutoBar";
+--AutoBar.panel.name = "AutoBar";
 -- Add the panel to the Interface Options
-InterfaceOptions_AddCategory(AutoBar.panel);
+--InterfaceOptions_AddCategory(AutoBar.panel);
 
--- Make a child panel
-AutoBar.buttonspanel = CreateFrame( "Frame", "AutoBarConfigButtons", AutoBar.panel);
-AutoBar.buttonspanel.name = "Buttons";
--- Specify childness of this panel (this puts it under the little red [+], instead of giving it a normal AddOn category)
-AutoBar.buttonspanel.parent = AutoBar.panel.name;
--- Add the child to the Interface Options
-InterfaceOptions_AddCategory(AutoBar.buttonspanel);
 
-AutoBar.foodpanel = CreateFrame( "Frame", "AutoBarConfigButtonsFood", AutoBar.buttonspanel);
-AutoBar.foodpanel.name = "Food";
--- Specify childness of this panel (this puts it under the little red [+], instead of giving it a normal AddOn category)
-AutoBar.foodpanel.parent = AutoBar.buttonspanel.name;
--- Add the child to the Interface Options
-InterfaceOptions_AddCategory(AutoBar.foodpanel);
+---- Make a child panel
+--AutoBar.buttonspanel = CreateFrame( "Frame", "AutoBarConfigButtons", AutoBar.panel);
+--AutoBar.buttonspanel.name = "Buttons";
+---- Specify childness of this panel (this puts it under the little red [+], instead of giving it a normal AddOn category)
+--AutoBar.buttonspanel.parent = AutoBar.panel.name;
+---- Add the child to the Interface Options
+--InterfaceOptions_AddCategory(AutoBar.buttonspanel);
+--
+--AutoBar.foodpanel = CreateFrame( "Frame", "AutoBarConfigButtonsFood", AutoBar.buttonspanel);
+--AutoBar.foodpanel.name = "Food";
+---- Specify childness of this panel (this puts it under the little red [+], instead of giving it a normal AddOn category)
+--AutoBar.foodpanel.parent = AutoBar.buttonspanel.name;
+---- Add the child to the Interface Options
+--InterfaceOptions_AddCategory(AutoBar.foodpanel);
+
+
+-- Debug Panel
+--AutoBar.buttonspanel = CreateFrame( "Frame", "AutoBarConfigDebug", AutoBar.panel);
+--AutoBar.buttonspanel.name = "Debug";
+
+--AutoBar.buttonspanel.parent = AutoBar.panel.name; -- Child of the main panel
+--InterfaceOptions_AddCategory(AutoBar.buttonspanel);
+
+--BlizOptionsGroup_AB_Name = "AutoBar2"
+local blizz_options = AceGUI:Create("BlizOptionsGroup")
+blizz_options:SetName("AutoBar2")
+InterfaceOptions_AddCategory(blizz_options.frame);
+
+local blizz_options_debug = AceGUI:Create("BlizOptionsGroup")
+blizz_options_debug:SetName("AutoBar2-1", "AutoBar2")
+InterfaceOptions_AddCategory(blizz_options_debug.frame);
