270035c13e4e7019a7e433292646c2a76cd0bba4
Ken Paulson <ken@muffinmangames.com>
2016-10-21 23:04:49 -0400

    Updated LibtPT-Muffin

--- libs/MuffinLibPTSets/LibPT-Muffin.Skill.Fishing.lua
+++ libs/MuffinLibPTSets/LibPT-Muffin.Skill.Fishing.lua
@@ -2,11 +2,11 @@
 -- LibPT-Muffin.Skill.Fishing
 --
 if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
-LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Skill.Fishing", "Rev: 7",
+LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Skill.Fishing", "Rev: 10",
 {
 	["Muffin.Skill.Fishing.Lure"] = "6529,6530,6532,6533,6811,7307,33820,34861,46006,62673,67404,68049,88710,117405,118391,124674,133707,133710,133712,133714,133717,133720,133722,133795,138956,138957,138958,138959,138960,138961,138962,138963,139175",
 	["Muffin.Skill.Fishing.Misc"] = "133704,133708,133709,133719,133723,138448,141975",
-	["Muffin.Skill.Fishing.Rare Fish.Legion"] = "139656,139657,139667,139669",
+	["Muffin.Skill.Fishing.Rare Fish.Legion"] = "139654,139656,139657,139661,139662,139663,139667,139669",
 })
 
 
