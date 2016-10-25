d63ce90d9a9f18734bcd4ede4c62a857540bd355
Ken Paulson <ken@muffinmangames.com>
2016-10-25 13:57:47 -0400

    Updated MuffinLibPT

---
+++ libs/MuffinLibPTSets/LibPT-Muffin.Toys.lua
@@ -0,0 +1,10 @@
+--
+-- LibPT-Muffin.Toys
+--
+if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
+LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Toys", "Rev: 1",
+{
+	["Muffin.Toys.Hearth"] = "54452,64488,93672",
+})
+
+
