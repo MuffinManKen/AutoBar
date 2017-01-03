c4f888a08aed5df3d5485830fa95d9ec65f66ce9
Ken Paulson <ken@muffinmangames.com>
2017-01-03 04:00:18 -0500

    Added compression settings

--- package.bat
+++ package.bat
@@ -3,7 +3,7 @@ cd ..
 
 del AutoBar.zip
 
-7z a AutoBar.zip AutoBar\ -xr!.git -xr!.gitignore -xr!*.ctag -xr!*.prj -xr!*.pui -xr!*.bat -xr!notes.txt -xr!xcopy_ignore.txt
+7z a AutoBar.zip AutoBar\ -mx9 -xr!.git -xr!.gitignore -xr!*.ctag -xr!*.prj -xr!*.pui -xr!*.bat -xr!notes.txt -xr!xcopy_ignore.txt
 
 
 
