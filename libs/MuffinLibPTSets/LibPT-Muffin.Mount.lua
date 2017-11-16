ef4abab9986e7638614985b95b60358c0736e27f
Ken Paulson <ken@muffinmangames.com>
2017-11-16 15:11:40 -0500

    Updated data libs

--- libs/MuffinLibPTSets/LibPT-Muffin.Mount.lua
+++ libs/MuffinLibPTSets/LibPT-Muffin.Mount.lua
@@ -5,6 +5,15 @@ if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded befo
 LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Mount", "Rev: 2",
 {
 	["Muffin.Mount.Deathknight"] = "-48778,-54729",
+	["Muffin.Mount.Demonhunter"] = "",
+	["Muffin.Mount.Druid"] = "",
+	["Muffin.Mount.Hunter"] = "",
+	["Muffin.Mount.Mage"] = "",
+	["Muffin.Mount.Monk"] = "",
 	["Muffin.Mount.Paladin"] = "-13819,-23214,-34767,-34769,-69820,-69826,-73629,-73630",
+	["Muffin.Mount.Priest"] = "",
+	["Muffin.Mount.Rogue"] = "",
+	["Muffin.Mount.Shaman"] = "",
 	["Muffin.Mount.Warlock"] = "-238454,-238452,-232412,-23161,-5784",
+	["Muffin.Mount.Warrior"] = "",
 })
