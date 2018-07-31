e850bb7db149b3c4cb761fd4e1daa659925ab290
Ken Paulson <ken@muffinmangames.com>
2018-07-30 22:40:08 -0400

    Prepping for release 7.9.9.03

--- Readme.txt
+++ Readme.txt
@@ -8,6 +8,10 @@ https://muffinmangames.com
 *For skinning install the Masque addon
 
 Changes:
+v7.9.9.03: BETA
+ - Improved handling of macro icons
+ - Hopefully fixed more stuff
+
 v7.9.9.02: BETA
  - BETA: This is Beta code, if that makes you nervous don't use it
  - Fixed AutoBar issue with Mounts
--- whatsnew.lua
+++ whatsnew.lua
@@ -5,7 +5,8 @@ local _, AB = ... -- Pulls back the Addon-Local Variables and store them locally
 local WHATSNEW_TEXT = "" ..
 [[
  - BETA: This is Beta code, if that makes you nervous don't use it
- - Fixed AutoBar issue with Mounts
+ - Improved handling of macro icons
+ - Hopefully fixed more stuff
 ]] .. "|n"
 
 function AB.show_whats_new()
