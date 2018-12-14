befa120085595b8db9b5e839c214dd54b6d95ced
Ken Paulson <ken@muffinmangames.com>
2018-12-14 06:45:40 -0500

    Fixed path to addons folder

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
-xcopy "D:\Projects\WoW\AutoBar" "E:\World of Warcraft\Interface\Addons\AutoBar" /Y /S /EXCLUDE:xcopy_ignore.txt
+xcopy "D:\Projects\WoW\AutoBar" "E:\World of Warcraft\_retail_\Interface\Addons\AutoBar" /Y /S /EXCLUDE:xcopy_ignore.txt
 
 pause
