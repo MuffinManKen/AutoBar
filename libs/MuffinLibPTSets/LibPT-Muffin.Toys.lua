18939599bf586ffab49a84add93626a6c22c6efc
Ken Paulson <ken@muffinmangames.com>
2019-08-02 21:34:02 -0400

    Updated libs

--- libs/MuffinLibPTSets/LibPT-Muffin.Toys.lua
+++ libs/MuffinLibPTSets/LibPT-Muffin.Toys.lua
@@ -2,12 +2,12 @@
 -- LibPT-Muffin.Toys
 --
 if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
-LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Toys", "Rev: 13",
+LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Toys", "Rev: 14",
 {
-	["Muffin.Toys.Hearth"] = "54452,64488,93672,142542,162973,163045,165669,165670,165802,166746,166747",
+	["Muffin.Toys.Hearth"] = "54452,64488,93672,142542,162973,163045,165669,165670,165802,166746,166747,168907",
 	["Muffin.Toys.Pet Battle"] = "92738,122681",
 	["Muffin.Toys.Companion Pet.Ornamental"] = "37460,44820,89222,127695,127696,127707,127766",
-	["Muffin.Toys.Portal.Engineer"] = "18984,18986,30542,30544,48933,87215,112059,151652",
+	["Muffin.Toys.Portal.Engineer"] = "18984,18986,30542,30544,48933,87215,112059,151652,167075,168807,168808",
 	["Muffin.Toys.Portal.Other"] = "95567,95568,136849,140324",
 	["Muffin.Toys.Fishing.Misc"] = "85500,85973",
 	["Muffin.Toys.Fishing.Bobbers"] = "142528,142529,142530,142531,142532,143662,147307,147308,147309,147310,147311,147312",
