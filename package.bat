0a22e4047258e015b55c5d19ca12c17d37764ec3
Ken Paulson <ken@muffinmangames.com>
2019-08-27 08:39:12 -0400

    Prepping for release 1.13.2.00

--- package.bat
+++ package.bat
@@ -3,7 +3,7 @@ cd ..
 
 del AutoBarClassic.zip
 
-7z a AutoBarClassic.zip AutoBar\ -mx9 -xr!.git -xr!.gitignore -xr!*.ctag -xr!*.prj -xr!*.pui -xr!*.bat -xr!notes.txt -xr!xcopy_ignore.txt
+7z a AutoBarClassic.zip AutoBar\ -mx9 -xr!.git -xr!.gitignore -xr!*.ctag -xr!*.prj -xr!*.pui -xr!*.bat -xr!notes.txt -xr!ChangeList.txt -xr!xcopy_ignore.txt
 
 
 
