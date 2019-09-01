6126681622f32e13eb90edbb547e3121803bb195
Ken Paulson <ken@muffinmangames.com>
2019-09-01 18:48:37 -0400

    Using new wow packager

--- package.bat
+++ package.bat
@@ -1,10 +1,6 @@
 
-cd ..
-
-del AutoBarClassic.zip
-
-7z a AutoBarClassic.zip AutoBar\ -mx9 -xr!.git -xr!.gitignore -xr!*.ctag -xr!*.prj -xr!*.pui -xr!*.bat -xr!notes.txt -xr!ChangeList.txt -xr!xcopy_ignore.txt
 
 
+..\WowPackager\wow_packager.py
 
 pause
