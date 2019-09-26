36405750aa384ec8e9ee3a606bbf73b640e40166
Ken Paulson <ken@muffinmangames.com>
2019-09-25 23:04:16 -0400

    Fixes for my packager to handle multiple packages

--- package.bat
+++ package.bat
@@ -1,10 +1,6 @@
 
-cd ..
-
-del AutoBar.zip
-
-7z a AutoBar.zip AutoBar\ -mx9 -xr!.git -xr!.gitignore -xr!*.ctag -xr!*.prj -xr!*.pui -xr!*.bat -xr!notes.txt -xr!xcopy_ignore.txt
 
 
+..\WowPackager\wow_packager.py %*
 
 pause
