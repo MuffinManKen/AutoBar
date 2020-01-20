6782dc4e29572d3674f0b910c1303f0afbdd61f4
Ken Paulson <ken@muffinmangames.com>
2020-01-20 12:07:36 -0500

    Fixed What's New bug Release 8.3.0.01

--- whatsnew.lua
+++ whatsnew.lua
@@ -17,6 +17,7 @@ else
 	[[
 	 - Updates for WoW 8.3
 	 - On the Hearth button, you can now choose to only show Favourite hearth toys
+	 - Fixed bug in the what`s new dialog
 	 - Go to MuffinManGames.com to report any issues or for information on my Patreon
 	]] .. "|n"
 
@@ -34,7 +35,7 @@ function AB.show_whats_new()
 		local q_entry = {}
 		q_entry.addon_name = ADDON_NAME
 		q_entry.addon_version = this_version
-		q_entry.body_text = text
+		q_entry.body_text = WHATSNEW_TEXT
 
 		MUFFIN_WHATS_NEW_QUEUE.AddEntry(q_entry)
 
