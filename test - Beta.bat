6ee1233464765e2e0edc1768925b1e6f261a3d87
ken_paulson <ken@muffinmangames.com>
2020-08-28 04:01:12 -0400

    admin files

---
+++ test - Beta.bat
@@ -0,0 +1,5 @@
+xcopy "." "E:\World of Warcraft\_beta_\Interface\Addons\AutoBar" /Y /S /I /EXCLUDE:xcopy_ignore_retail.txt+xcopy_ignore.txt
+
+REM robocopy "." "E:\World of Warcraft\_retail_\Interface\Addons\AutoBar" /s
+
+pause
