61063b88e767dfef5e5ab2cce1bed67245ed4091
ken_paulson <ken@muffinmangames.com>
2020-11-12 18:24:05 -0500

    Prepping for 9.0.1.03

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,6 +3,9 @@ local ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store the
 
 AB.WHATSNEW_TEXT = "" ..
 [[
+ - Fixed broken file in 9.0.1.02
+ - Mage: Arcane Intellect
+ - Challenge ports are split out into their own category and the Hearth button has an option to disable them
  - Rogues: Instant Poison
  - More work on fixing Crafting buttons
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
