69d713cdf9e40d59c7ffbf090067cf2cd7361e55
ken_paulson <ken@muffinmangames.com>
2020-11-27 13:13:44 -0500

    Prepping for v9.0.2.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -5,6 +5,7 @@ AB.WHATSNEW_TEXT = "" ..
 [[
  - Added new Herbs to Milling button
  - Added new fishing bait
+ - Added Fishing Poles
  - Added Mage portal
  - General data mining
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
