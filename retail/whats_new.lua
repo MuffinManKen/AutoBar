92a58509301e6912bc743ea13f89b067e6aed291
ken_paulson <ken@muffinmangames.com>
2020-12-06 04:57:51 -0500

    Prepping for v9.0.2.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,11 +3,9 @@ local ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store the
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Added new Herbs to Milling button
- - Added new fishing bait
- - Added Fishing Poles
- - Added Mage portal
- - General data mining
+ - Removed the Mana and Cooldown Mana buttons, since Recovery does the same thing
+ - Added Mana Stones to Recovery, and Create Mana Stones to Mage's Conjure button
+ - Added an Anima category
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
 
