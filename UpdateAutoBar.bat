e09b6283b9d08980eb711c9d4ab44d618fe0e172
ken_paulson <ken@muffinmangames.com>
2020-12-22 09:59:52 -0500

    Datamined more stuff: Covenant Hearthstones, Engineer Portals, Buff items, Bandages, etc

--- UpdateAutoBar.bat
+++ UpdateAutoBar.bat
@@ -5,9 +5,9 @@ xcopy "D:\Projects\Current\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Consumable"
 xcopy "D:\Projects\Current\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Misc" ".\libs\LibPeriodicTable-3.1-Misc" /Y /S /I
 xcopy "D:\Projects\Current\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Tradeskill" ".\libs\LibPeriodicTable-3.1-Tradeskill" /Y /S /I
 
-xcopy "D:\Projects\Current\WoW\ace3\AceConfig-3.0" ".\libs\AceConfig-3.0"  /Y /S /I
-xcopy "D:\Projects\Current\WoW\ace3\AceConsole-3.0" ".\libs\AceConsole-3.0"  /Y /S /I
-xcopy "D:\Projects\Current\WoW\ace3\AceGUI-3.0" ".\libs\AceGUI-3.0"  /Y /S /I
+xcopy "D:\Projects\Current\WoW\libs\ace3\AceConfig-3.0" ".\libs\AceConfig-3.0"  /Y /S /I
+xcopy "D:\Projects\Current\WoW\libs\ace3\AceConsole-3.0" ".\libs\AceConsole-3.0"  /Y /S /I
+xcopy "D:\Projects\Current\WoW\libs\ ace3\AceGUI-3.0" ".\libs\AceGUI-3.0"  /Y /S /I
 
 copy "D:\Projects\Current\WoW\MuffinUIToolkit\muffin_ui_toolkit.lua" ".\libs\"
 copy "D:\Projects\Current\WoW\MuffinWhatsNew\muffin_whats_new.lua" ".\libs\"
@@ -15,6 +15,6 @@ copy "D:\Projects\Current\WoW\MuffinWhatsNew\muffin_whats_new.lua" ".\libs\"
 copy "D:\Projects\Current\WoW\MuffinIcons\*.tga" ".\textures\"
 
 
-xcopy "D:\Projects\Current\WoW\libkeybound-1-0\LibKeyBound-1.0" ".\libs\LibKeyBound-1.0" /Y /S /I
+xcopy "D:\Projects\Current\WoW\libs\libkeybound-1-0\LibKeyBound-1.0" ".\libs\LibKeyBound-1.0" /Y /S /I
 
 pause
