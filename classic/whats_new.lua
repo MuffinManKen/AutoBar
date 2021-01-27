f37303779a49d8523025017b8093cbd94e408885
ken_paulson <ken@muffinmangames.com>
2021-01-27 09:42:53 -0500

    Prepping for v1.13.6.00

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,10 +3,9 @@ local ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store the
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - A bunch of minor fixes
- - Minor locale cleanup
- - Fixed issues with Rogue Poisons showing up on the Weapon buff button
- - Custom Categories are visible again in the Categories tab
- - When adding Categories to a Custom button, Categories are now sorted!
+ - Whipper Root Tuber is no longer Food
+ - Druid: Added Dire Bear Form
+ - Lots of behind the scenes stuff
+ - TOC bump
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
