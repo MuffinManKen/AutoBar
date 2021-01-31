f296892833539605203d0a2f4686c11e9c919be2
ken_paulson <ken@muffinmangames.com>
2021-01-31 11:20:01 -0500

    Retail: Added missing fishing items

--- retail/MuffinLibPTSets/LibPT-Muffin.Toys.lua
+++ retail/MuffinLibPTSets/LibPT-Muffin.Toys.lua
@@ -2,7 +2,7 @@
 -- LibPT-Muffin.Toys
 --
 if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
-LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Toys", "Rev: 17",
+LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Toys", "Rev: 18",
 {
 	["Muffin.Toys.Hearth"] = "54452, 64488, 93672, 142542, 162973, 163045, 163206, 165669, 165670, 165802, 166746, 166747, 168907, 172179, 180290, 182773, 183716, 184353",
 	["Muffin.Toys.Pet Battle"] = "92738,122681",
@@ -10,5 +10,5 @@ LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Toys", "Rev: 17",
 	["Muffin.Toys.Portal.Engineer"] = "18984, 18986, 30542, 30544, 48933, 87215, 112059, 151652, 168807, 168808, 172924",
 	["Muffin.Toys.Portal.Other"] = "95567,95568,136849,140324",
 	["Muffin.Toys.Fishing.Misc"] = "85500,85973",
-	["Muffin.Toys.Fishing.Bobbers"] = "142528,142529,142530,142531,142532,143662,147307,147308,147309,147310,147311,147312",
+	["Muffin.Toys.Fishing.Bobbers"] = "142528, 142529, 142530, 142531, 142532, 143662, 147307, 147308, 147309, 147310, 147311, 147312, 180993",
 })
