e6bf0db28f9566d61edbc8ec21afcaa82271b328
ken_paulson <ken@muffinmangames.com>
2021-02-25 10:04:17 -0500

    Prepping for release 9.0.2.04

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,10 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Added some missing fishing items
+ - Fixed: Battle Pet button shouldn't have duplicates
+ - Fixed: Hack Pet Action Frame should be more reliable
+ - Minor performance improvements
+ - Lots of behind-the-scenes maintenance
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
 
