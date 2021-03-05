fef910747bf21bbaecf10e11c67378379dd882f3
ken_paulson <ken@muffinmangames.com>
2021-03-05 14:11:32 -0500

    Prepping v9.0.2.05

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,10 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fixed: Battle Pet button shouldn't have duplicates
- - Fixed: Hack Pet Action Frame should be more reliable
- - Minor performance improvements
- - Lots of behind-the-scenes maintenance
+ - Fixed: Custom Categories were not showing up in the Category list
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
 
