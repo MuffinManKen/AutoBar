bdeac11aeea99a93dd7f0a97a5215749dd280d18
ken_paulson <ken@muffinmangames.com>
2021-03-05 14:42:12 -0500

    Prepping for release v1.13.6.01

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,9 +3,9 @@ local ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store the
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Whipper Root Tuber is no longer Food
- - Druid: Added Dire Bear Form
- - Lots of behind the scenes stuff
- - TOC bump
+ - Fixed: Custom Categories were not showing up in the Category list
+ - Fixed: Hack Pet Action Frame should be more reliable
+ - Minor performance improvements
+ - Lots of behind-the-scenes maintenance
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
