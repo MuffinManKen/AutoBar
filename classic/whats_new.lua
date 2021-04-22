92487c2217ee1398ad576d45a84420056369d6f4
ken_paulson <ken@muffinmangames.com>
2021-04-21 22:33:48 -0400

    Prepping for v1.13.7.00

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,9 +3,8 @@ local ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store the
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fixed: Custom Categories were not showing up in the Category list
- - Fixed: Hack Pet Action Frame should be more reliable
- - Minor performance improvements
- - Lots of behind-the-scenes maintenance
+ - Fixed warning about missing sets
+ - Fixed a typo that kept Greater Soulstone spell from appearing
+ - Possible fix for Warlock and Paladin mounts not appearing
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
