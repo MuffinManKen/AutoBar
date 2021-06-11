0edb2aca2779aecb4af9dbf75e6e72e81cb4674d
ken_paulson <ken@muffinmangames.com>
2021-06-11 03:59:58 -0400

    Prepping v2.05.1.03

--- bcc/whats_new.lua
+++ bcc/whats_new.lua
@@ -3,8 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
-	- Priest: Inner Fire, Shadow Protection, and Prayer of Shadow Protection
-	- Shaman: Windwall Totem and Mana Tide Totem
-	- Warlock: Summon Felguard
+	- Increased the width of the UI widgets for button config so less text is cutoff
+	- Added new "Square Popup" option to control whether popups are built to be square-ish or taller while still respecting max popup size.  Default is True, which is the current behaviour.
+	- Shaman: Moar Totems! (Wrath of Air, Earth Elemental, Wrath)
 	- Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
