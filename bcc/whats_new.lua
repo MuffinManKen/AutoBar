a6338ebe1dd29cf9a5bb79389d3a261909d0e57a
ken_paulson <ken@muffinmangames.com>
2021-06-23 13:26:10 -0400

    Prepping v2.05.1.05

--- bcc/whats_new.lua
+++ bcc/whats_new.lua
@@ -3,11 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
-	- More data mining
-	- Hunter: Aspect of the Viper
-	- Mage: Fixed Invisibility
-	- Added Grid2LayoutFrame as a Sticky Frame
-	- Warlock: Fel Armor
 	- Druid: Flight Form
 	- Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
