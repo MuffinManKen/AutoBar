68ffe51f46d0fe0631238acb05e43bec665cf31d
ken_paulson <ken@muffinmangames.com>
2021-06-26 01:03:37 -0400

    Prepping v1.13.7.02

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,7 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Priest: Inner Fire, Shadow Protection, and Prayer of Shadow Protection
- - Shaman: Windwall Totem and Mana Tide Totem
+ - Fixed Paladin & Warlock mounts
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
