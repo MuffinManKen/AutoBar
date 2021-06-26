72007e2c94c7c7535a08f76125a5d01cde88fd93
ken_paulson <ken@muffinmangames.com>
2021-06-26 01:12:49 -0400

    Prepping v2.05.1.06

--- bcc/whats_new.lua
+++ bcc/whats_new.lua
@@ -3,6 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
-	- Druid: Flight Form
+	- Hunter: Added Snake Trap, Fixed Intimidation icon
+	- Paladin/Warlock: Fixed mounts
+	- Mage: Mana Emerald
 	- Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
