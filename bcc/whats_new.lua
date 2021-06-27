caa21f15303620a619246714bade4ff8f34e3747
ken_paulson <ken@muffinmangames.com>
2021-06-27 15:40:08 -0400

    Prepping v2.05.1.07

--- bcc/whats_new.lua
+++ bcc/whats_new.lua
@@ -3,8 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
-	- Hunter: Added Snake Trap, Fixed Intimidation icon
-	- Paladin/Warlock: Fixed mounts
-	- Mage: Mana Emerald
-	- Go to MuffinManGames.com to report any issues or for information on my Patreon
+ - For many spells that have a single & group version, the group version is on the Right Click of the single version
+ - More datamining
+ - Paladin: Crusader Aura & Seal of the Martyr
+ - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
