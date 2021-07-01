f8985ecbd0e9c6a03757cf0be8a2083856973c57
ken_paulson <ken@muffinmangames.com>
2021-07-01 12:13:40 -0400

    Prepping for v2.05.1.08

--- bcc/whats_new.lua
+++ bcc/whats_new.lua
@@ -3,8 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - For many spells that have a single & group version, the group version is on the Right Click of the single version
- - More datamining
- - Paladin: Crusader Aura & Seal of the Martyr
+ - Priest: Race-specfic abilites & Shadowfiend
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
