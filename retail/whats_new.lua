c46fb92ffc23d50e91ee0d80f8712bae7fca07d9
ken_paulson <ken@muffinmangames.com>
2021-07-15 23:38:34 -0400

    Prepping for v9.0.5.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,10 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - TOC Bump
+ - Removed out-of-date Healthstone Category. Use Muffin.Stones.Health instead.
+ - Paladin: Concentration Aura
+ - Added new Category: Muffin.Misc.Repair (Auto Hammers and stuff)
+ - General datamining
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
 
