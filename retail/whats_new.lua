51307cc5128c0e1f8eb3f4c093655268f5022d59
ken_paulson <ken@muffinmangames.com>
2021-07-23 08:39:23 -0400

    Prepping for v9.1.0.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -6,6 +6,8 @@ AB.WHATSNEW_TEXT = "" ..
  - Removed out-of-date Cooldown.Drums Category. Use Muffin.Drums instead.
  - Updated LibPeriodicTable
  - Performance improvements
+ - Added Resonating Anima Core/Mote to Quest items
+ - Fixed Mounts and Move Bars/Buttons
  - Go to MuffinManGames.com to report any issues or for information on my Patreon
 ]] .. "|n"
 
