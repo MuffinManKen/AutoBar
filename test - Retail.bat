8925d83d0e1c96468a6cda8ba751f1ebe0f96f4f
ken_paulson <ken@muffinmangames.com>
2022-03-31 02:17:07 -0400

    Updated path to retail install

--- test - Retail.bat
+++ test - Retail.bat
@@ -1,4 +1,4 @@
-xcopy "." "E:\World of Warcraft\_retail_\Interface\Addons\AutoBar" /Y /S /I /EXCLUDE:xcopy_ignore_retail.txt+xcopy_ignore.txt
+xcopy "." "D:\World of Warcraft\_retail_\Interface\Addons\AutoBar" /Y /S /I /EXCLUDE:xcopy_ignore_retail.txt+xcopy_ignore.txt
 
 REM robocopy "." "E:\World of Warcraft\_retail_\Interface\Addons\AutoBar" /s
 
