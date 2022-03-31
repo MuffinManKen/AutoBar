0f02285804e349d9cca5588418de7fa6071441c2
ken_paulson <ken@muffinmangames.com>
2022-03-31 02:27:06 -0400

    Prepping for release 9.2.0.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,10 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Improvements to handling of Battle Pet Favourites
- - This is my final release
+ - TOC Update
+ - Fixed really long line in Summon Battle Pet Fave description
+ - Fixed broken "Move Buttons" from the main GUI
+ - NOTE: I'm not back, just felt bad about the broken Move Buttons issue
 ]] .. "|n"
 
 
