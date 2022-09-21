0a37872080af5511343a4d6694da9aa0ed707d45
Ken Paulson <ken@muffinmangames.com>
2022-09-21 17:49:10 -0400

    Removed extraneous test script Changed absolute paths to relative in update script

--- Beta test.bat
+++
@@ -1,3 +0,0 @@
-xcopy "D:\Projects\WoW\AutoBar" "E:\World of Warcraft Beta\Interface\AddOns\AutoBar" /Y /S /EXCLUDE:xcopy_ignore.txt
-
-pause
--- UpdateAutoBar.bat
+++ UpdateAutoBar.bat
@@ -1,22 +1,22 @@
-xcopy "D:\Projects\Current\WoW\MuffinLibPTSets\classic" ".\classic\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
-xcopy "D:\Projects\Current\WoW\MuffinLibPTSets\bcc" ".\bcc\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
-xcopy "D:\Projects\Current\WoW\MuffinLibPTSets\wrath" ".\wrath\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
-xcopy "D:\Projects\Current\WoW\MuffinLibPTSets\retail" ".\retail\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
+xcopy "..\MuffinLibPTSets\classic" ".\classic\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
+xcopy "..\MuffinLibPTSets\bcc" ".\bcc\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
+xcopy "..\MuffinLibPTSets\wrath" ".\wrath\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
+xcopy "..\MuffinLibPTSets\retail" ".\retail\MuffinLibPTSets" /Y /S /I /EXCLUDE:xcopy_ignore.txt
 
-xcopy "D:\Projects\Current\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Consumable" ".\libs\LibPeriodicTable-3.1-Consumable" /Y /S /I
-xcopy "D:\Projects\Current\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Misc" ".\libs\LibPeriodicTable-3.1-Misc" /Y /S /I
-xcopy "D:\Projects\Current\WoW\LibPeriodicTable\LibPeriodicTable-3.1-Tradeskill" ".\libs\LibPeriodicTable-3.1-Tradeskill" /Y /S /I
+xcopy "..\LibPeriodicTable\LibPeriodicTable-3.1-Consumable" ".\libs\LibPeriodicTable-3.1-Consumable" /Y /S /I
+xcopy "..\LibPeriodicTable\LibPeriodicTable-3.1-Misc" ".\libs\LibPeriodicTable-3.1-Misc" /Y /S /I
+xcopy "..\LibPeriodicTable\LibPeriodicTable-3.1-Tradeskill" ".\libs\LibPeriodicTable-3.1-Tradeskill" /Y /S /I
 
-xcopy "D:\Projects\Current\WoW\libs\ace3\AceConfig-3.0" ".\libs\AceConfig-3.0"  /Y /S /I
-xcopy "D:\Projects\Current\WoW\libs\ace3\AceConsole-3.0" ".\libs\AceConsole-3.0"  /Y /S /I
-xcopy "D:\Projects\Current\WoW\libs\ ace3\AceGUI-3.0" ".\libs\AceGUI-3.0"  /Y /S /I
+xcopy "..\libs\ace3\AceConfig-3.0" ".\libs\AceConfig-3.0"  /Y /S /I
+xcopy "..\libs\ace3\AceConsole-3.0" ".\libs\AceConsole-3.0"  /Y /S /I
+xcopy "..\libs\ ace3\AceGUI-3.0" ".\libs\AceGUI-3.0"  /Y /S /I
 
-copy "D:\Projects\Current\WoW\MuffinUIToolkit\muffin_ui_toolkit.lua" ".\libs\"
-copy "D:\Projects\Current\WoW\MuffinWhatsNew\muffin_whats_new.lua" ".\libs\"
+copy "..\MuffinUIToolkit\muffin_ui_toolkit.lua" ".\libs\"
+copy "..\MuffinWhatsNew\muffin_whats_new.lua" ".\libs\"
 
-copy "D:\Projects\Current\WoW\MuffinIcons\*.tga" ".\textures\"
+copy "..\MuffinIcons\*.tga" ".\textures\"
 
 
-xcopy "D:\Projects\Current\WoW\libs\libkeybound-1-0\LibKeyBound-1.0" ".\libs\LibKeyBound-1.0" /Y /S /I
+xcopy "..\libs\libkeybound-1-0\LibKeyBound-1.0" ".\libs\LibKeyBound-1.0" /Y /S /I
 
 pause
