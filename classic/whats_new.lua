95573f89a141254d5ad44461bbc56f477613809b
Ken Paulson <ken@muffinmangames.com>
2022-09-24 11:18:41 -0400

    Classic: Updated TOC

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,7 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Performance improvements
- - Minor fixes
- - This is my final release
+ - Updated data
+ - TOC Update
+ - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
