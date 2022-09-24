66690ba9a0d9b976c0de56e85b90c924e8e93d3f
Ken Paulson <ken@muffinmangames.com>
2022-09-24 12:24:24 -0400

    Retail: Datamining

--- retail/MuffinLibPTSets/LibPT-Muffin.Drum.lua
+++ retail/MuffinLibPTSets/LibPT-Muffin.Drum.lua
@@ -2,7 +2,7 @@
 -- Muffin.Potions
 --
 if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
-LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Drum", "Rev: 2",
+LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Drum", "Rev: 3",
 {
-	["Muffin.Drum.Haste"] = "29529, 102351, 120257, 142406, 154167, 172233, 193470",
+	["Muffin.Drum.Haste"] = "29529, 102351, 120257, 142406, 154167, 172233",
 })
