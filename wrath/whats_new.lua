6c9a75bc80e7b175fe2022c9d1abb0f332ae2bcc
Ken Paulson <ken@muffinmangames.com>
2022-09-29 22:02:01 -0400

    Release v3.00.4.05

--- wrath/whats_new.lua
+++ wrath/whats_new.lua
@@ -3,8 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Professions: Added Milling button
- - Items: Updated Hearth entries
- - Priest: Divine Spirit, Prayer of Spirit (jasskin)
+ - Mage: Added Teleport/Portal Dalaran
+ - Items: Hearth-like items (Direbew, etc) should be there again
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
