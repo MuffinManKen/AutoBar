f3c4ed113d1f0520be7666e324f84c9b9a350267
Ken Paulson <ken@muffinmangames.com>
2022-10-05 10:53:34 -0400

    Release v3.00.4.06

--- wrath/whats_new.lua
+++ wrath/whats_new.lua
@@ -3,7 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Mage: Added Teleport/Portal Dalaran
- - Items: Hearth-like items (Direbew, etc) should be there again
+ - Warrior: Added Commanding Shout alternating with Battle Shout
+ - Battlegrounds: Possible fix for issues people having been happening at the start of BGs
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
