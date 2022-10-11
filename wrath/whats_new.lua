b0e9fe775082124c23c8e2559e05033e1dc42935
Ken Paulson <ken@muffinmangames.com>
2022-10-11 04:48:22 -0400

    Release v3.00.4.07

--- wrath/whats_new.lua
+++ wrath/whats_new.lua
@@ -3,7 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Warrior: Added Commanding Shout alternating with Battle Shout
- - Battlegrounds: Possible fix for issues people having been happening at the start of BGs
+ - Fix for Battlegrounds/Arena fix
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
