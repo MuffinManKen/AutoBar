714338be564f10721999f5a77d1a5666944ff978
Ken Paulson <ken@muffinmangames.com>
2022-10-13 09:27:59 -0400

    Release v3.00.4.07a

--- wrath/whats_new.lua
+++ wrath/whats_new.lua
@@ -3,6 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fix for Battlegrounds/Arena fix
+ - Lots of behind-the-scenes changes that will be required for Dragonflight
+ - Backup your settings!!!!
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
