bf6830d464ca09059558dc2c17a3dacecce5755a
Ken Paulson <ken@muffinmangames.com>
2022-10-14 23:36:32 -0400

    Release v3.00.4.07b

--- wrath/whats_new.lua
+++ wrath/whats_new.lua
@@ -5,5 +5,6 @@ AB.WHATSNEW_TEXT = "" ..
 [[
  - Lots of behind-the-scenes changes that will be required for Dragonflight
  - Backup your settings!!!!
+ - A few more fixes since the previous alpha
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
