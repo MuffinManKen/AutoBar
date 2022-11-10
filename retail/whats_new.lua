0b0ec463d747e208b29e9c57c556057bb83d7376
Ken Paulson <ken@muffinmangames.com>
2022-11-10 17:20:16 -0500

    Release v10.0.0.05

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Many setting-related fixes
+ - Fixed some astonishingly stupid errors. Sorry. I feel very dumb.
  - If you're still having issues with settings being lost, please attach your saved variables to an issue so I can see them
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
