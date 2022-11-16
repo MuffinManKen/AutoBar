f6f08d40b642513ca94dca81ca4c991ff728e794
Ken Paulson <ken@muffinmangames.com>
2022-11-16 08:05:15 -0500

    Release v10.0.2.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,14 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fixed some astonishingly stupid errors. Sorry. I feel very dumb.
- - If you're still having issues with settings being lost, please attach your saved variables to an issue so I can see them
+ - TOC bump
+ - Fixed bug when mount names weren't available after Hearthing
+ - Performance improvements
+ - Hearth toys default to Favourites only
+ - Temporarily removed ToyBox button
+ - Reworked lots of stuff so buttons with toys (like Hearth) are more reliable
+ - If you're still having issues with settings being lost or bars/buttons missing,
+         please attach your saved variables to an issue so I can see them
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
 
