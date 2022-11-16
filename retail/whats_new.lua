271d9e1008a65fcc63d93962b69916d15b139883
Ken Paulson <ken@muffinmangames.com>
2022-11-16 15:09:24 -0500

    Release v10.0.2.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,12 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - TOC bump
- - Fixed bug when mount names weren't available after Hearthing
- - Performance improvements
- - Hearth toys default to Favourites only
- - Temporarily removed ToyBox button
- - Reworked lots of stuff so buttons with toys (like Hearth) are more reliable
+ - Evoker fixes
  - If you're still having issues with settings being lost or bars/buttons missing,
          please attach your saved variables to an issue so I can see them
  - I'm not playing WoW, so if things are missing make sure you report them.
