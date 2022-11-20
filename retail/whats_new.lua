826ddcbb52254382f0e43b0ba64de66918db246c
Ken Paulson <ken@muffinmangames.com>
2022-11-20 10:29:48 -0500

    Release v10.0.2.04

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fixed non-Hearth toys not showing up
- - Fixed invalid account field error
+ - Another fix for bars not appearing (bad argument #1 to 'pairs')
+ - Tweaked some logging options
  - If you're still having issues with settings being lost or bars/buttons missing,
          please attach your saved variables to an issue so I can see them
  - I'm not playing WoW, so if things are missing make sure you report them.
