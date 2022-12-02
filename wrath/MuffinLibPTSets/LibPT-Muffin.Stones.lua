3c4470a7ae622bac7e2d1551403436db12549c35
Ken Paulson <ken@muffinmangames.com>
2022-12-02 05:43:15 -0500

    Updated some mining

--- wrath/MuffinLibPTSets/LibPT-Muffin.Stones.lua
+++ wrath/MuffinLibPTSets/LibPT-Muffin.Stones.lua
@@ -2,8 +2,8 @@
 -- LibPT-Muffin.Stones
 --
 if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
-LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Stones", "Rev: 6",
+LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Stones", "Rev: 7",
 {
-	["Muffin.Stones.Mana"] = "5513, 5514, 8007, 8008, 22044",
+	["Muffin.Stones.Mana"] = "5513, 5514, 8007, 8008, 22044, 33312",
 	["Muffin.Stones.Health"] = "5509, 5510, 5511, 5512, 9421, 19004, 19005, 19006, 19007, 19008, 19009, 19010, 19011, 19012, 19013, 22103, 22104, 22105, 36889, 36890, 36891, 36892, 36893, 36894, 42462",
 })
