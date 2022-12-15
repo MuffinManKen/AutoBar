e029565c4013c55b01ab102438e81ced0a0f64a4
Ken Paulson <ken@muffinmangames.com>
2022-12-15 10:39:29 -0500

    Release v10.0.2.06

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,10 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Added Journals to Crafting button
- - Added Valdrakken portal
  - Did some data mining
- - Added code to cache Toy data so toys will hopefully show up more reliably.
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
