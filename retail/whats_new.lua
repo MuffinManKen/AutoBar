4dad22e4c4357ea19cab3e0579993ddd993fbe9d
Ken Paulson <ken@muffinmangames.com>
2023-01-02 16:43:30 -0500

    Prepping release v10.0.2.07

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,10 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Did some data mining
+ - Druid: added Mark of the Wild
+ - Updated datamining
+ - Added Evokers to the list of Mana users
+ - Trying a different approach with tracking mounts. Is it better?
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
