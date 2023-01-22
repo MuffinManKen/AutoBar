dcce441a3bef5fbf1fe6cab6000f8e9fe23bf79f
Ken Paulson <ken@muffinmangames.com>
2023-01-22 09:38:12 -0500

    Wrath release v3.04.1.00

--- wrath/whats_new.lua
+++ wrath/whats_new.lua
@@ -3,6 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fix for Battlegrounds/Arena fix
+ - Fix API changes
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
