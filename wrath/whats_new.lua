a1d8882bc7211e08bae990b1179b04cb57d33b79
Ken Paulson <ken@muffinmangames.com>
2023-01-27 15:58:22 -0500

    Release v3.04.1.01

--- wrath/whats_new.lua
+++ wrath/whats_new.lua
@@ -3,6 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fix API changes
+ - Backed out a stupid hack that was required for Dragonflight, but made Wrath double-click stuff
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
