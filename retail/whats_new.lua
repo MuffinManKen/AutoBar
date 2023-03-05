8f401273ebaf268a2ac0ba7ff1313fbc72e1d649
Ken Paulson <ken@muffinmangames.com>
2023-03-05 12:49:43 -0500

    Prepping v10.0.2.08

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,10 +3,9 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Druid: added Mark of the Wild
  - Updated datamining
- - Added Evokers to the list of Mana users
- - Trying a different approach with tracking mounts. Is it better?
+ - Added Class Buff for Evokers
+ - Added 2 new Categories: Muffin.ItemEnchant.Permanent & Temporary
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
