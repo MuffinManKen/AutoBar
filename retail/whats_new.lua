6ed22326f77b0133573b89dbd9039d1bcac44976
Ken Paulson <ken@muffinmangames.com>
2023-03-26 09:09:34 -0400

    Prepping for v10.0.7.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -4,8 +4,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 AB.WHATSNEW_TEXT = "" ..
 [[
  - Updated datamining
- - Added Class Buff for Evokers
- - Added 2 new Categories: Muffin.ItemEnchant.Permanent & Temporary
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
