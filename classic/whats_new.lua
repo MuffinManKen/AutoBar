479a84e7aeb3f519a1ab8a74007f55480c60be7b
Ken Paulson <ken@muffinmangames.com>
2023-11-11 10:17:06 -0500

    forgotten Classic whats_new update

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,7 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Updated data
+ - API updates
  - TOC Update
  - If you'd like to see development continue, consider supporting the Kickstarter:
  https://www.kickstarter.com/projects/2075655491/autobar-2023
