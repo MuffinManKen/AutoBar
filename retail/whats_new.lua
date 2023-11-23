7c6cc4c5262c490c677c3d4b826102b8ce32625d
Ken Paulson <ken@muffinmangames.com>
2023-11-23 01:09:47 -0500

    Prepping v10.2.0.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,9 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - TOC Bump
- - Fixed datamining
+ - Added Niffen Diggin Mitts
+ - Added Amplifying Poison for Rogues
+ - Possibly fixed Mount sorting issue
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
