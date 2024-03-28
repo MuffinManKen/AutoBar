c047509a21d3deb42689f971f00fa3015791698a
Ken Paulson <ken@muffinmangames.com>
2024-03-27 21:28:14 -0400

    Version 10.2.6.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,9 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Added Niffen Diggin Mitts
- - Added Amplifying Poison for Rogues
- - Possibly fixed Mount sorting issue
+ - Fix for error caused by the latest patch.  Thanks to Asjaskan on Curseforge for the assist.
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
