d88de67b6cc574b57626ac7e82c66ee9ff1858df
Ken Paulson <ken@muffinmangames.com>
2024-05-15 14:29:46 -0400

    Classic TOC update

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,7 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Updated dataming
- - Fixed Hunter's Track button
+ - Hopefully fixed issue with WoW's latest release
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
