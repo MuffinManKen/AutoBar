f239959d71cc53efdbab896d9b66a292c17d2702
ken_paulson <ken@muffinmangames.com>
2024-07-12 16:50:34 -0400

    v4.00.4.01 - Datamined items

--- cata/whats_new.lua
+++ cata/whats_new.lua
@@ -3,6 +3,6 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Added Aspect of the Dragonhawk
+ - Datamined Items
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
