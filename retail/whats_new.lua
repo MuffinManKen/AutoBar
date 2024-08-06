441ba9bef32b10982b8e8708c43fca58249b9aed
ken_paulson <ken@muffinmangames.com>
2024-08-05 22:07:05 -0400

    v11.0.0.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fix for error caused by the latest patch.
+ - Fix not being able to drag bars
+ - Fix not being able to keybind
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
