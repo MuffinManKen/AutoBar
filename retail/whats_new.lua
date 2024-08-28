dc5de069b12711840dbd025ed5d513ed71e38bbe
ken_paulson <ken@muffinmangames.com>
2024-08-28 03:08:39 -0400

    Retail v11.0.2.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fixes for War Within
+ - Updated datamining
+ - Added Dornogal Teleportals for Mages (thanks dperschonok)
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
