4979cbf32547c35ed96beebc0a924f120b63e272
ken_paulson <ken@muffinmangames.com>
2024-08-30 23:06:03 -0400

    v11.0.2.02

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Updated datamining
- - Added Dornogal Teleportals for Mages (thanks dperschonok)
+ - Fix for error when dropping spells onto custom categories
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
