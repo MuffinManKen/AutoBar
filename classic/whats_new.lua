4fa5fdaa276ea5bb21942c430205376770601480
ken_paulson <ken@muffinmangames.com>
2024-10-09 19:31:46 -0400

    v1.15.5.00

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,6 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Cleaned up class abilities thanks to Alessandro Barbieri
+ - Toc Bump
+ - Fixed issue with Keybinding
+ - Datamining
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
