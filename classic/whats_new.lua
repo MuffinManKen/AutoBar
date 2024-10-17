9dacc83ff6b9c08ab15f0d3bd81d5790baac42e4
ken_paulson <ken@muffinmangames.com>
2024-10-16 22:54:07 -0400

    v1.15.4.01

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,8 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Toc Bump
- - Fixed issue with Keybinding
- - Datamining
+ - Warlock: Added Fel Armor
+ - Added Beacon of Hydraxis to Hearth button
+ - Improved datamining of food and drinks
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
