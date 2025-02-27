209262a91c6905f36b1fdd08fc5aa0451e8b651f
ken_paulson <ken@muffinmangames.com>
2025-02-27 16:59:36 -0500

    Flogging my game

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -5,6 +5,7 @@ AB.WHATSNEW_TEXT = "" ..
 [[
  - TOC Bump
  - Data mining
+ - Check out my new game The Map Is The Thing on Steam!
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
