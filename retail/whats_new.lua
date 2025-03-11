5f3ff07a1984724efa5f3f54723300157ebf456c
ken_paulson <ken@muffinmangames.com>
2025-03-11 07:56:13 -0400

    v11.1.0.02

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Hide the weird triangle that appears over buttons on the bar
+ - Hide the weird triangle that appears over buttons on the flyouts
  - Check out my new game |cFFD100FFThe Map Is The Thing|r on Steam!
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
