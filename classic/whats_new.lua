e53f19b6be1ec7cebb2690e16f3bdd33c0e5e1d1
ken_paulson <ken@muffinmangames.com>
2025-06-08 23:28:39 -0400

    v1.15.7.00

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,7 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Priest: Added Levitate and Divine Spirit
- - Fixed error when creating a new bar
+ - TOC Bump and datamining
+ - Rogue: Should be able to use the Pick Lock button again
+ - Check out my new game |cFFD100FFThe Map Is The Thing|r on Steam!
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
