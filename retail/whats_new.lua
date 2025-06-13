f63db02a85ecb13b95e8dcfa421dd5a52917686a
ken_paulson <ken@muffinmangames.com>
2025-06-13 14:55:45 -0400

    datamining

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,8 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Hide the weird triangle that appears over buttons on the flyouts
+ - TOC Bump
+ - Datamining
  - Check out my new game |cFFD100FFThe Map Is The Thing|r on Steam!
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
