0c5b1be9a8460b53de3598068456ca85dc4f52c5
ken_paulson <ken@muffinmangames.com>
2025-08-18 11:47:34 -0400

    v11.2.0.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - TOC Bump
- - Datamining
+ - Unbroke some stuff that my last update broke.  Sorry for the button flickering when you log in.
  - Check out my new game |cFFD100FFThe Map Is The Thing|r on Steam!
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
