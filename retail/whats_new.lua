074156657e5d3c32ea2e4a31f1d0949c71d74263
ken_paulson <ken@muffinmangames.com>
2025-08-19 03:10:18 -0400

    v11.2.0.02

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,7 +3,7 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Unbroke some stuff that my last update broke.  Sorry for the button flickering when you log in.
+ - More work on performance and Toy reliability.  Had to disable Only Favourites for now.
  - Check out my new game |cFFD100FFThe Map Is The Thing|r on Steam!
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
