067d3257ca4f0937b7fd12ccbdadca2c26fdd996
ken_paulson <ken@muffinmangames.com>
2025-08-22 09:32:20 -0400

    v11.2.0.03

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,10 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - More work on performance and Toy reliability.  Had to disable Only Favourites for now.
+ - Fixed a bug with button updates
+ - Shaman: Imbues, Shields, and Class Buffs (thanks to Illuminat442)
  - Check out my new game |cFFD100FFThe Map Is The Thing|r on Steam!
+ - Seriously. Check it out.  I need to feed my cats.
  - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
 
