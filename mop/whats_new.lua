f716de8facc7542b915bf2f20db57b32f333edfa
ken_paulson <ken@muffinmangames.com>
2025-09-04 11:25:59 -0400

    v5.05.0.02

--- mop/whats_new.lua
+++ mop/whats_new.lua
@@ -3,5 +3,9 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fixed missing TOC file
+ - Added Cata/MoP Mage Portals (thanks to Kate Karnage)
+ - Added LibPT set Muffin.Misc.Hearth
+ - Check out my new game |cFFD100FFThe Map Is The Thing|r on Steam!
+ - Seriously. Check it out.  I need to feed my cats.
+ - I'm not playing WoW, so if things are missing or broken make sure you report them with as much detail as you can.
 ]] .. "|n"
