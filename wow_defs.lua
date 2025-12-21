ad5e18cbf6aa187ef12cef0ba590acd3b862fb07
ken_paulson <ken@muffinmangames.com>
2025-12-21 10:19:17 -0500

    Track faction ids for mounts to filter out mounts only usable by other factions

--- wow_defs.lua
+++ wow_defs.lua
@@ -172,6 +172,7 @@ ABCode = {}
 ---@field CLASS string The player's class in all caps without whitespace. Locale-independent.
 ---@field NiceClass string The player's class with the first letter capitalized and the rest lower without whitespace. Locale-independent.
 ---@field player_faction_name string The player's faction name (Alliance/Horde/Neutral).  Mixed-case and locale-independent.
+---@field player_faction_id number 0 == Horde, 1 == Alliance
 ---@field frame Frame Frame used for keybinding and event handling
 ---@field current_player string Formatted as "Player - Realm"
 ---@field version string Version string of Addon as given by the TOC
