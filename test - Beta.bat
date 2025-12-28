eb805d2d268e486b66e81cd8bb165be88a87e2cb
ken_paulson <ken@muffinmangames.com>
2025-12-28 15:57:34 -0500

    Update test script for beta

--- test - Beta.bat
+++ test - Beta.bat
@@ -1,5 +1,39 @@
-xcopy "." "%WOW_INSTALL_DIR%\_beta_\Interface\Addons\AutoBar" /Y /S /I /EXCLUDE:xcopy_ignore_retail.txt+xcopy_ignore.txt
-xcopy "." "%WOW_INSTALL_DIR%\_ptr_\Interface\Addons\AutoBar" /Y /S /I /EXCLUDE:xcopy_ignore_retail.txt+xcopy_ignore.txt
+
+set SRC="."
+set DST="%WOW_INSTALL_DIR%\_beta_\Interface\Addons\AutoBar"
+
+robocopy %SRC% %DST%  /E ^
+  /XF ^
+    *.bat ^
+    .gitignore .luacheckrc ^
+    autobar.code-workspace ^
+    AutoBar*.toc ^
+    *.py ^
+    *.txt ^
+	 *.json ^
+	 wow_defs.lua ^
+	 *_*.tga ^
+  /XD ^
+    .github ^
+    .git ^
+    .vscode ^
+    bcc ^
+	 cata ^
+	 classic^
+	 docs ^
+	 mop ^
+	 tools
+
+robocopy %SRC% %DST% AutoBar.toc
+pause
+exit
+
+robocopy ".\libs" "%DST%\libs" /MIR /E
+robocopy ".\locale" "%DST%\locale" /MIR /E
+robocopy ".\retail" "%DST%\retail" /MIR /E
+robocopy ".\retail" "%DST%\retail" /MIR /E
+robocopy ".\textures" "%DST%\textures" /MIR /E /XF *_*
+robocopy "." "%DST%" /MIR /LEV:1 /XF .gitignore .luacheckrc autobar.code-workspace AutoBarBCC.toc AutoBarClassic* *.bat *.py *.txt *.json wow_defs.lua
 
 
 pause
--- test - Retail.bat
+++ test - Retail.bat
@@ -28,12 +28,13 @@ robocopy %SRC% %DST% AutoBar.toc
 pause
 exit
 
-robocopy ".\libs" "%WOW_INSTALL_DIR%\_retail_\Interface\Addons\AutoBar\libs" /MIR /E 
-robocopy ".\locale" "%WOW_INSTALL_DIR%\_retail_\Interface\Addons\AutoBar\locale" /MIR /E 
-robocopy ".\retail" "%WOW_INSTALL_DIR%\_retail_\Interface\Addons\AutoBar\retail" /MIR /E 
-robocopy ".\retail" "%WOW_INSTALL_DIR%\_retail_\Interface\Addons\AutoBar\retail" /MIR /E 
-robocopy ".\textures" "%WOW_INSTALL_DIR%\_retail_\Interface\Addons\AutoBar\textures" /MIR /E /XF *_*
-robocopy "." "%WOW_INSTALL_DIR%\_retail_\Interface\Addons\AutoBar" /MIR /LEV:1 /XF .gitignore .luacheckrc autobar.code-workspace AutoBarBCC.toc AutoBarClassic* *.bat *.py *.txt *.json wow_defs.lua
+
+robocopy ".\libs" "%DST%\libs" /MIR /E
+robocopy ".\locale" "%DST%\locale" /MIR /E
+robocopy ".\retail" "%DST%\retail" /MIR /E
+robocopy ".\retail" "%DST%\retail" /MIR /E
+robocopy ".\textures" "%DST%\textures" /MIR /E /XF *_*
+robocopy "." "%DST%" /MIR /LEV:1 /XF .gitignore .luacheckrc autobar.code-workspace AutoBarBCC.toc AutoBarClassic* *.bat *.py *.txt *.json wow_defs.lua
 
 
 pause
