9cf75c1ff58c236848fb458e4804a70110b48340
ken_paulson <ken@muffinmangames.com>
2025-12-30 14:09:57 -0500

    v12.0.0.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,11 +3,11 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
-AutoBar needs your help to survive the Midnight expansion!
-Blizzard's upcoming changes will break AutoBar. Because I no longer play WoW myself, continued support depends on community interest.
-
-A Kickstarter is now live to fund a full Midnight update + one year of support.
-https://www.kickstarter.com/projects/2075655491/autobar-midnight-update-world-of-warcraft
+ - Initial support for Midnight Beast
+ - Datamining for Mignight
+ - Added Housing Category and Button
+ - Improved Mount handling
+ - Small performance improvement
 ]] .. "|n"
 
 
