d5ae9de056af9871feb8af611ce16701a3428e15
ken_paulson <ken@muffinmangames.com>
2026-01-03 13:58:50 -0500

    Update script no longer updates muffin_whats_new, since I don't have multiple addons anymore.

--- UpdateAutoBar.bat
+++ UpdateAutoBar.bat
@@ -9,7 +9,7 @@ xcopy "..\libs\ace3\AceConsole-3.0" ".\libs\AceConsole-3.0"  /Y /S /I
 xcopy "..\libs\ace3\AceGUI-3.0" ".\libs\AceGUI-3.0"  /Y /S /I
 
 copy "..\MuffinUIToolkit\muffin_ui_toolkit.lua" ".\libs\"
-copy "..\MuffinWhatsNew\muffin_whats_new.lua" ".\libs\"
+REM copy "..\MuffinWhatsNew\muffin_whats_new.lua" ".\libs\"
 
 copy "..\MuffinIcons\muffin.tga" ".\textures\"
 
