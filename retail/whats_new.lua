47343c85f23a96aa0b07735b0090fbd7a72b6609
ken_paulson <ken@muffinmangames.com>
2026-01-06 14:36:00 -0500

    v12.0.0.01

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,11 +3,11 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Initial support for Midnight Beast
- - Datamining for Mignight
- - Added Housing Category and Button
- - Improved Mount handling
- - Small performance improvement
+ - Fixed issue when Hunters tame (probably happened in other contexts too)
+ - New Category: Muffin.Misc.CraftingKnowledge
+ - Openables button optionally shows CraftingKnowledge items
+ - Updated Ace3 library
+ - Added Muffin.Misc.Hearth to Retail for non-toy portally things
 ]] .. "|n"
 
 
