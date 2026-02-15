13f307071554533eef243e3a8b7bf08c2cfb88be
ken_paulson <ken@muffinmangames.com>
2026-02-15 10:18:54 -0500

    v12.0.1.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,9 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Fixed issue with new 'secret' values in the API.
- - Added names of Midnight Patrons. If your name is missing, please check if you've answered the survey.
+ - TOC bump
+ - Added remaining Kickstarter supporter names
+ - Datamining
 ]] .. "|n"
 
 
