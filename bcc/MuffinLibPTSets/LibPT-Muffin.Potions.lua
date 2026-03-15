4e09796d3293ab91b7c38d15574e1d07dced4418
ken_paulson <ken@muffinmangames.com>
2026-03-14 23:56:43 -0400

    TBC: Mana/Health injectors should be under potions, not food. Oops.

--- bcc/MuffinLibPTSets/LibPT-Muffin.Potions.lua
+++ bcc/MuffinLibPTSets/LibPT-Muffin.Potions.lua
@@ -2,10 +2,10 @@
 -- Muffin.Potions
 --
 if not LibStub("LibPeriodicTable-3.1", true) then error("PT3 must be loaded before data") end
-LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Potion", "Rev: 27",
+LibStub("LibPeriodicTable-3.1"):AddData("Muffin.Potion", "Rev: 28",
 {
-	["Muffin.Potion.Health"] = "118, 858, 929, 1710, 3928, 4596, 13446, 17348, 17349, 18839, 22829, 23579, 28100, 31676, 32763, 32905, 32947, 33934",
-	["Muffin.Potion.Mana"] = "2455, 3087, 3385, 3827, 6149, 13443, 13444, 17351, 17352, 18841, 22832, 23578, 28101, 31677, 32762, 32902, 32948, 33935",
+	["Muffin.Potion.Health"] = "118, 858, 929, 1710, 3928, 4596, 13446, 17348, 17349, 18839, 22829, 23579, 23822, 28100, 31676, 32763, 32905, 32947, 33092, 33934",
+	["Muffin.Potion.Mana"] = "2455, 3087, 3385, 3827, 6149, 13443, 13444, 17351, 17352, 18841, 22832, 23578, 23823, 28101, 31677, 32762, 32902, 32948, 33093, 33935",
 	["Muffin.Potion.Combo"] = "2456, 9144, 18253, 22850, 34440",
 	["Muffin.Potion.Buff"] = "3384, 9036, 22828",
 	["Muffin.Potion.Rage"] = "5631, 5633, 13442",
