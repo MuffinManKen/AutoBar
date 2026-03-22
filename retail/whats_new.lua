c99929ffe625f49084430f8c1e14eb22aca866b2
ken_paulson <ken@muffinmangames.com>
2026-03-22 01:49:37 -0400

    v12.0.1.06

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -3,8 +3,12 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
- - Added Crests as Openables
- - Hopefully improved the handling of Cooldowns
+ - On the fishing button, you can right-click to to open the Fishing Journal
+ - Added non-toy bobbers and Darkmoon Fishing Cap.
+ - Added more portal-like things.
+ - Changing Clamp To Screen setting on Bars no longer requires a ui reload
+ - Fixed a bug where Re-arrange on use wasn't working properly.
+ - Control-clicking a popup button when the button is set to Rearrange on use, changes the default button without actually using the item/spell.
 ]] .. "|n"
 
 
