381508fcc6e3f04bccc938c9400edbebd784d97d
ken_paulson <ken@muffinmangames.com>
2026-07-29 04:51:17 -0400

    v12.0.7.00

--- classic/whats_new.lua
+++ classic/whats_new.lua
@@ -3,6 +3,10 @@ local _ADDON_NAME, AB = ... -- Pulls back the Addon-Local Variables and store th
 
 AB.WHATSNEW_TEXT = "" ..
 [[
+ - TOC bump
  - Fixed issue with buttons not responding thanks to Daniel Shookowsky
+ - Datamining
+ - FIX:Renaming Bars works
+ - Have you checked out Lexomancer on Steam or Itch.io? You should! Free demo...
  - I'm not playing WoW, so if things are missing make sure you report them.
 ]] .. "|n"
