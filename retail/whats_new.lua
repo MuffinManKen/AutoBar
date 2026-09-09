0e990fa36ec3c190e02dd3f8d288cbe0a00b09c1
ken_paulson <ken@muffinmangames.com>
2026-09-08 20:45:48 -0400

    v12.1.0.00

--- retail/whats_new.lua
+++ retail/whats_new.lua
@@ -5,8 +5,8 @@ AB.WHATSNEW_TEXT = "" ..
 [[
  - TOC bump
  - Datamining
- - FIX:Renaming Bars works
- - Also scan the Reagent Bag, if it exists
+ - Updated thrid-party libs
+ - Fixed issue with other bars blocking AutoBar's in MoveBars mode
  - Have you checked out Lexomancer on Steam or Itch.io? You should! Free demo...
 ]] .. "|n"
 
